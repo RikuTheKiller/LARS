cd036b548d826bd94286317d523139f98634867a
Riku Leinonen <vihervaara100@gmail.com>
2023-02-10 22:00:24 +0200

    Primitive behaviour code

--- mod.cs
+++ mod.cs
@@ -18,16 +18,35 @@ namespace Mod
 
     }
 
-    public class LimbAttachmentWireTool : DistanceWireTool
+    public class AttachmentWireTool : DistanceWireTool
     {
         protected override void OnJointCreate(DistanceJoint2D joint)
         {
-            
+            AttachmentWireBehaviour attachmentWireBehaviour = gameObject.AddComponent<AttachmentWireBehaviour>();
         }
     }
 
-    public class LimbAttachmentWireBehaviour : DistanceJointWireBehaviour
+    public class AttachmentWireBehaviour : DistanceJointWireBehaviour
     {
+        HingeJoint2D joint;
+        LimbBehaviour limb;
+        LimbBehaviour connectedLimb;
+        CirculationBehaviour circulation;
+        CirculationBehaviour connectedCirculation;
 
+        protected override void Created()
+        {
+            base.Created();
+            joint = gameObject.GetComponent<HingeJoint2D>();
+            limb = gameObject.GetComponent<LimbBehaviour>();
+            connectedLimb = joint.connectedBody.GetComponent<LimbBehaviour>();
+            circulation = gameObject.GetComponent<CirculationBehaviour>();
+            connectedCirculation = gameObject.GetComponent<CirculationBehaviour>();
+            if (!joint || !limb || !connectedLimb || !circulation || !connectedCirculation)
+            {
+                Destroy(this);
+                return;
+            }
+        }
     }
 }
