d838361a659165ce69408d1f87e30ca724b732d4
Riku Leinonen <vihervaara100@gmail.com>
2023-02-10 19:21:21 +0200

    Boilerplate

---
+++ mod.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Mod
+{
+    public class Mod : MonoBehaviour
+    {
+        public const string ModTag = " [LARS]";
+
+        public static void Main()
+        {
+
+        }
+    }
+
+    public static class ModUtils
+    {
+        
+    }
+}
