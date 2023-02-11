db31da5cf0e19cc440ea27c349090f673313f07d
Riku Leinonen <vihervaara100@gmail.com>
2023-02-11 20:50:36 +0200

    Apparently I never removed that

--- main.cs
+++ main.cs
@@ -17,41 +17,4 @@ namespace Mod
     {
 
     }
-
-    public class AttachmentWireTool : DistanceWireTool
-    {
-        protected override void OnJointCreate(DistanceJoint2D joint)
-        {
-            AttachmentWireBehaviour attachmentWireBehaviour = gameObject.AddComponent<AttachmentWireBehaviour>();
-        }
-    }
-
-    public class AttachmentWireBehaviour : DistanceJointWireBehaviour
-    {
-        HingeJoint2D joint;
-        LimbBehaviour limb;
-        LimbBehaviour connectedLimb;
-        CirculationBehaviour circulation;
-        CirculationBehaviour connectedCirculation;
-        ConnectedNodeBehaviour nodeBehaviour;
-        ConnectedNodeBehaviour connectedNodeBehaviour;
-
-        protected override void Created()
-        {
-            base.Created();
-            joint = gameObject.GetComponent<HingeJoint2D>();
-            limb = gameObject.GetComponent<LimbBehaviour>();
-            connectedLimb = joint.connectedBody.GetComponent<LimbBehaviour>();
-            circulation = gameObject.GetComponent<CirculationBehaviour>();
-            connectedCirculation = joint.connectedBody.GetComponent<CirculationBehaviour>();
-            nodeBehaviour = gameObject.GetComponent<ConnectedNodeBehaviour>();
-            connectedNodeBehaviour = joint.connectedBody.GetComponent<ConnectedNodeBehaviour>();
-            
-            if (!joint || !limb || !connectedLimb || !circulation || !connectedCirculation || !nodeBehaviour || !connectedNodeBehaviour)
-            {
-                Destroy(this);
-                return;
-            }
-        }
-    }
 }
